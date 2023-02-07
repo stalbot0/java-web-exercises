96d5441fdb6f1d83ffc4107b34a11b099c5868e9
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-07 10:14:25 -0600

    first commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) { // <'main'> <tab>
+        // void means it doesn't return anything, this is the entry point into this program
+        System.out.println("Hello World"); // <'sout'> <tab> creates this -- similar to console.log()
+    }
+}
+//the target folder is always changing as we change our program. We don't share the target folder when we push to github
