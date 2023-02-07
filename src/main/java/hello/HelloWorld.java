1ec1c751f0d694c6f593486bc134d5f92c9c7612
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-07 10:52:44 -0600

    intro java exercise complete

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,7 +3,9 @@ package hello;
 public class HelloWorld {
     public static void main(String[] args) { // <'main'> <tab>
         // void means it doesn't return anything, this is the entry point into this program
-        System.out.println("Hello World"); // <'sout'> <tab> creates this -- similar to console.log()
+        System.out.println("Hello World");
+        System.out.print("Hello World\n"); // <'sout'> <tab> creates this -- similar to console.log()
+        System.out.print("Hello World");
     }
 }
 //the target folder is always changing as we change our program. We don't share the target folder when we push to github
