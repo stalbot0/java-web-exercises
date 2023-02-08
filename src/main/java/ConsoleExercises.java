52393b9c86a6b63b5fde173b1b7b7ef76b7df905
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-08 09:56:22 -0600

    added new exercise and completed the last exercise

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -7,81 +7,55 @@ public class ConsoleExercises {
 //        // we can use format to re-format our value into a string.
 //        String newPi = String.format("%.2f", pi);
 //        System.out.printf("The value of pi is approximately %s%n", newPi);
-//
-        Scanner input = new Scanner(System.in);
-//
+
+        Scanner input = new Scanner(System.in); //System.in tells the scanner to use the keyboard
+
 //        System.out.print("Enter an Integer: ");
-//        int userNum = input.nextInt();
-//        System.out.printf("Your integer is: %d%n", userNum);
+//        int userInt = input.nextInt();
+//        System.out.printf("Your integer is: %d%n", userInt);
 //        //if the user enters something that is not an integer, we get errors bc of the int declaration
-//
 //        input.nextLine();
-//
+
 //        System.out.print("Please enter three words separated by one space each: ");
 //        String userFirstWord = input.next();
 //        String userSecondWord = input.next();
 //        String userThirdWord = input.next();
-//
-//        System.out.printf("Your first word is %s%n", userFirstWord);
-//        System.out.printf("Your second word is %s%n", userSecondWord);
-//        System.out.printf("Your third word is %s%n", userThirdWord);
-//
+//        System.out.printf("Your first word is %s%nYour second word is %s%nYour second word is %s%n", userFirstWord, userSecondWord, userThirdWord);
 //        input.nextLine();
-//
+
 //        System.out.print("Please enter a sentence: ");
 //        String userSentence = input.nextLine();
 //        System.out.printf("You entered: %s%n", userSentence);
-//
 //        input.nextLine();
 
-        System.out.print("Please enter the length AND width AND height of your classroom in feet: ");
-        double roomLength = input.nextDouble();
-        double roomWidth = input.nextDouble();
-        double roomHeight = input.nextDouble();
-        double roomArea = roomLength * roomWidth;
-        double roomPerimeter = (roomHeight * 2) + (roomLength * 2);
+        System.out.print("Enter width: ");
+//        String wid = input.nextLine();
+//        double width = Double.parseDouble(wid);
+        double width = input.nextDouble();
 
-        input.nextLine();
+        System.out.print("Enter length: ");
+//        String len = input.nextLine();
+//        double length = Double.parseDouble(len);
+        double length = input.nextDouble();
 
-        System.out.printf("The area of your room is %.2fft%n", roomArea);
-        System.out.printf("The perimeter of your room is %.2fft%n", roomPerimeter);
+        double area = length * width;
+        double perimeter = (length * 2) + (width * 2);
+        System.out.printf("""
+                Length: %.1f
+                Width: %.1f
+                Area: %.1f
+                Perimeter: %.1f%n
+                """
+                , length, width, area, perimeter); //use triple quotes to use multiple lines, and gets rid of %n
 
-        //currently does not work with decimals
+//        input.useDelimiter("ouch"); //this allows us to denote what is used to separate tokens
+
+        System.out.print("Enter height: ");
+        double height = input.nextDouble();
+
+        double volume = length * width * height;
+        System.out.printf("The volume of the room is %.1f", volume);
+
         input.close();
     }
 }
-
-//    //NOTES FROM LECTURE
-////        int a = 5;
-////        String myName = "Stephen";
-////        double c = 12.57;
-////
-////        System.out.println("Howdy " + myName + ", age: " + a + " makes " + c + " bucks per hour");
-////        System.out.printf("Howdy %s, age: %d makes %f bucks per hour%n", myName, a, c);
-//    Scanner input = new Scanner(System.in);
-//
-//        System.out.print("Enter your age: ");
-//
-//                int age = input.nextInt();
-//                System.out.println("Your age is " + age);
-//
-//                input.nextLine(); //this resets the "enter" key so that the console won't run through to completion. Comment this out and run it to see what I mean
-//
-//                System.out.print("Enter your name: ");
-//
-////        String aName = input.next();
-////        System.out.println("Your first name is: " + aName);
-////
-////        aName = input.next();
-////        System.out.println("Your last name is: " + aName);
-//
-//                String newName = input.nextLine();
-//                System.out.println("Your name is " + newName);
-//
-////        System.out.print("Enter your age: ");
-////
-////        int age = input.nextInt();
-////        System.out.println("Your age is " + age);
-//
-//                input.close(); // close the program when finished with it
-////create the scanner outside the loop, and close it outside the loop
