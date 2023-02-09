5356e85a4db35ad5dac2840cc3b8c52e3622322f
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-09 11:50:13 -0600

    multi line java strings

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner bob = new Scanner(System.in);
+        String byeBob;
         System.out.println("Start a conversation with Bob...");
         do {
             System.out.print("You: ");
-            String askingBob = bob.nextLine();
+            byeBob = bob.nextLine();
 
             String bobsResponse;
-            if (askingBob.endsWith("?")) {
+            if (byeBob.endsWith("?")) {
                 bobsResponse = "Bob: Sure.";
-            } else if (askingBob.endsWith("!")) {
+            } else if (byeBob.endsWith("!")) {
                 bobsResponse = "Bob: Whoa. Chill out!";
-            } else if (askingBob.length() == 0) {
+            } else if (byeBob.trim().length() == 0) {
                 bobsResponse = "Bob: Fine, be that way!";
             } else {
                 bobsResponse = "Bob: Whatever.";
             }
             System.out.println(bobsResponse);
 
-            System.out.print("Continue the conversation? [Yes/No]: ");
-            String contConvo = bob.nextLine();
-
-            if(contConvo.equalsIgnoreCase("no")) {
+            if(byeBob.equalsIgnoreCase("bye")) {
                 System.out.println("Bob: Bye, nerd.");
                 break;
             }
