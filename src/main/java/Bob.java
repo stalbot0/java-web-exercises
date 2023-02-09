b3f0b59e3b17b17ccf852a3916c0d20b9f8128a3
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-09 09:41:30 -0600

    string exercise complete

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner bob = new Scanner(System.in);
+        do {
+            System.out.print("Say or ask ANYTHING to Bob: ");
+            String askingBob = bob.nextLine();
 
+            String bobsResponse;
+            if (askingBob.endsWith("?")) {
+                bobsResponse = "Bob: Sure.";
+            } else if (askingBob.endsWith("!")) {
+                bobsResponse = "Bob: Whoa. Chill out!";
+            } else if (askingBob.length() == 0) {
+                bobsResponse = "Fine, be that way!";
+            } else {
+                bobsResponse = "Whatever.";
+            }
+            System.out.println(bobsResponse);
+
+            System.out.println("Continue the conversation? [Yes/No]");
+            String contConvo = bob.nextLine();
+            if(!contConvo.equalsIgnoreCase("yes")) {
+                System.out.println("Bob: Bye, nerd.");
+                break;
+            }
+        } while (true);
     }
 }
