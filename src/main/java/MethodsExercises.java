5341cad4f9657da4c3a9eecc4c8128f40e0104b9
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-10 14:53:21 -0600

    methods number 3 done

--- src/main/java/MethodsExercises.java
+++ src/main/java/MethodsExercises.java
@@ -26,6 +26,21 @@ public class MethodsExercises {
         return int1 % int2;
     }
 
+    //si or no?
+    public static boolean yesOrNo() {
+        Scanner yesOrNoInput = new Scanner(System.in);
+        System.out.print("Would you like to continue? [Yes/No]: ");
+        String userInput = yesOrNoInput.nextLine();
+        if(userInput.equalsIgnoreCase("yes")) {
+            return true;
+        } else if (userInput.equalsIgnoreCase("no")) {
+            return false;
+        } else {
+            System.out.println("This is not a valid input");
+            return yesOrNo();
+        }
+    }
+
     //2. method validation
     public static int getInteger(int min, int max) {
         Scanner input = new Scanner(System.in);
@@ -38,22 +53,33 @@ public class MethodsExercises {
     }
 
     //3. calculate factorial
-    public static long calcFactorial(int min, int max) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer between 1 and 10: ");
-        int userInput = input.nextInt();
-        long factorial = 1;
-
+    public static long calcFactorial(int userInput) {
+        long factorial;
+        factorial = 1;
         for (int i = 1; i <= userInput; i++) {
             factorial *= i;
         }
-
-        System.out.print("Would you like to continue? [Y/N]: ");
-        String userCont = input.nextLine();
         return factorial;
     }
+//re-factor to use recursion
+//        int userInput;
+//        long factorial;
+//
+//        System.out.print("Enter an integer between 1 and 10: ");
+//        userInput = input.nextInt();
+//        if (userInput >= min && userInput <= max) {
+//            factorial = 1;
+//            for (int i = 1; i <= userInput; i++) {
+//                factorial *= i;
+//            }
+//            return factorial;
+//        }
+//        return calcFactorial(min, max);
+//    }
+    //4. dice-roller
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         //1
         System.out.println(MethodsExercises.addition(1, 0));
         System.out.println(MethodsExercises.subtraction(8, 6));
@@ -61,14 +87,25 @@ public class MethodsExercises {
         System.out.println(MethodsExercises.division(16, 4));
         System.out.println(MethodsExercises.modulus(15, 3));
 
-        //2
-        int userInput = MethodsExercises.getInteger(1, 10);
-        System.out.println("This is your number: " + userInput);
+//        //2
+//        int userInput = MethodsExercises.getInteger(1, 10);
+//        System.out.println("This is your number: " + userInput);
 
-        //3
-        long factorial = MethodsExercises.calcFactorial(1, 10);
-        System.out.println("This is your factorial: " + factorial);
+//        //3
+//        long factorial = MethodsExercises.calcFactorial(userInput);
+//        System.out.println("This is your factorial: " + factorial);
 
+        //2 and 3 combined
+        boolean yesNoMaybe;
+        do {
+            int userInput = MethodsExercises.getInteger(1, 10);
+            System.out.println("This is your number: " + userInput);
+            long factorial = MethodsExercises.calcFactorial(userInput);
+            System.out.println("This is your factorial: " + factorial);
+            yesNoMaybe = yesOrNo();
+        } while (yesNoMaybe);
+
+        //4
     }
 
 
