de61383c1518421af9fb78af108a952d6369df20
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-13 10:34:54 -0600

    added bonus to HighLow

--- src/main/java/HighLow.java
+++ src/main/java/HighLow.java
@@ -7,18 +7,25 @@ public class HighLow {
         Random newRandomNum = new Random();
         int newestRandomNum = newRandomNum.nextInt(100) + 1;
 //        System.out.println(newestRandomNum); // output random num to ensure that our method is working
-        System.out.println("I'm thinking of a number between 1 and 100. Try to guess what my number is.. ");
+        System.out.println("I'm thinking of a number between 1 and 100. Try to guess what my number is.. You have 5 tries. ");
         boolean goodGuess = true;
+        int userGuess = 0;
         do {
             int userNum = MethodsExercises.getInteger(1, 100);
 
             if (userNum == newestRandomNum) {
-                System.out.printf("GOOD GUESS!, %d was my number!", newestRandomNum);
+                System.out.printf("GOOD GUESS!, %d was my number!%n", newestRandomNum);
                 break;
             } else if (userNum > newestRandomNum) {
                 System.out.println("LOWER");
             } else {
                 System.out.println("HIGHER");
+            }
+            userGuess++;
+
+            if(userGuess > 5) {
+                System.out.printf("Too many guesses, my number was %d", newestRandomNum);
+                break;
             }
         } while (true);
         return newestRandomNum;
