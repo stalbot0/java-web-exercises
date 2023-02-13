e784ae64f3a3b5bfb79b1782b4ff27a455fdee10
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-13 15:02:09 -0600

    input validation class done

--- src/main/java/util/Input.java
+++ src/main/java/util/Input.java
@@ -3,5 +3,66 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+    private Scanner scanner;
 
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.scanner.nextLine();
+    }
+
+    boolean yesNo() {
+        String booleanInput = this.scanner.nextLine();
+        return booleanInput.equalsIgnoreCase("y") || booleanInput.equalsIgnoreCase("yes");
+    }
+
+    int getInt(int min, int max) {
+        System.out.printf("Enter a number between %d and %d: ", min, max);
+        int userInt = this.scanner.nextInt();
+        if (userInt >= min && userInt <= max) {
+            return userInt;
+        } else {
+            System.out.println("This is not a valid input.");
+        }
+        return getInt(min, max);
+    }
+
+    int getInt() {
+        System.out.print("Enter an integer: ");
+        return this.scanner.nextInt();
+    }
+
+    double getDouble(double min, double max) {
+        System.out.printf("Enter a number between %f and %f: ", min, max);
+        double userDouble = this.scanner.nextInt();
+        if (userDouble >= min && userDouble <= max) {
+            return userDouble;
+        } else {
+            System.out.println("This is not a valid input.");
+        }
+        return getDouble(min, max);
+    }
+
+    double getDouble() {
+        System.out.print("Enter an integer: ");
+        return this.scanner.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        Input myInput = new Input();
+
+        System.out.println(myInput.getString());
+
+        System.out.println(myInput.yesNo());
+
+        myInput.getInt(1, 50);
+
+        myInput.getInt();
+
+        myInput.getDouble(1, 50);
+
+        myInput.getDouble();
+    }
 }
