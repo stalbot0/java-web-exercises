43ee7fab73fcda6413e571f59085afeea9654c43
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-15 09:34:56 -0600

    fixed serverNameGenerator syntax

--- src/main/java/ServerNameGenerator.java
+++ src/main/java/ServerNameGenerator.java
@@ -2,18 +2,20 @@ import java.util.Random;
 
 public class ServerNameGenerator {
     private static String[] adjectives = {"amicable", "understanding", "volatile", "insolent", "taciturn", "cordial", "reserved", "ominous", "bleak", "petulant",};
+
     private static String[] nouns = {"geese", "crime", "zebra", "insurance", "basketball", "flower", "cup", "wave", "wax", "songs",};
 
-    private static String randomElement(String[] passedArr) {
-        Random randomGenerator = new Random();
-        int randomWord = randomGenerator.nextInt(passedArr.length);
+    private static Random randomNumGenerator = new Random();
+
+    public static String randomElement(String[] passedArr) {
+        int randomWord = randomNumGenerator.nextInt(passedArr.length);
         return passedArr[randomWord]; // returns the value of the passed arr at the random index
     }
 
     public static String serverName(String[] arr1, String[] arr2) {
         String adjective = randomElement(arr1);
         String noun = randomElement(arr2);
-        return adjective + "-" + noun;
+        return "Here is your server name:\n" + adjective + "-" + noun;
     }
 
     public static void main(String[] args) {
