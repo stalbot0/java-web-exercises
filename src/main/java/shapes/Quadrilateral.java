6e29487290c8840f75d2db5b3d38b57b41e1528d
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-16 15:08:54 -0600

    updated syntax

--- src/main/java/shapes/Quadrilateral.java
+++ src/main/java/shapes/Quadrilateral.java
@@ -4,17 +4,21 @@ public abstract class Quadrilateral extends Shape implements Measurable{
     protected double length;
     protected double width;
 
-    public Quadrilateral(double length, double width) {
-        this.length = length;
-        this.width = width;
+    public Quadrilateral(double len, double wid) {
+        length = len;
+        width = wid;
     }
 
     //abstract accessors
-    public abstract double getLength();
+    public double getLength() {
+        return length;
+    };
 
     public abstract void setLength(double length);
 
-    public abstract double getWidth();
+    public double getWidth() {
+        return width;
+    };
 
     public abstract void setWidth(double width);
 }
