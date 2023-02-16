6eeb4f571a849335bbf619111a3710064959f9c5
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-16 16:21:07 -0600

    updated syntax

--- src/main/java/shapes/ShapesTest.java
+++ src/main/java/shapes/ShapesTest.java
@@ -3,12 +3,13 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Measurable myShape = new Square(6);
+        Measurable myShape;
+        myShape = new Square(6);
         System.out.println(myShape.getArea());
         System.out.println(myShape.getPerimeter());
 
-        Measurable myShape2 = new Rectangle(3, 4);
-        System.out.println(myShape2.getArea());
-        System.out.println(myShape2.getPerimeter());
+        myShape = new Rectangle(3, 4);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
     }
 }
